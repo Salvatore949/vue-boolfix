64c0d7bd0477863362c1842bc418d63b6c9cad23
Salvatore949 <salvo.mess49@gmail.com>
2021-12-02 13:20:42 +0100

    printed values

---
+++ src/components/Cards.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="film_list">
+    <h2>{{details.original_title}}</h2>
+    <p>{{details.title}}</p>
+    <p>{{details.vote_average}}</p>
+    <p>{{details.original_language}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Cards',
+  props:{
+      details:Object,
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,17 +1,50 @@
 <template>
-  <header>
-      <h1>BOOLFLIX</h1>
-      <input type="text" id="choose" placeholder="HarryPotter">
-  </header>
+  <div>
+    <header>
+        <h1>BOOLFLIX</h1>
+        <input type="text" id="choose" placeholder="HarryPotter">
+    </header>
+    <div>
+    <Cards
+    v-for="film in results"
+    :key="film.id"
+    :details="film"
+    />      
+    </div>
+  </div>  
 </template>
 
 <script>
+import axios from "axios";
+import Cards from '@/components/Cards.vue'
+
 export default {
   name: 'Main',
-}
-data(){
-    return
-}
+  components:{
+    Cards
+  },
+  data(){
+    return{
+      apiUrl: "https://api.themoviedb.org/3/search/movie?api_key=c53a2a603471b7e3608f9dee04a90392&query=harry+potter",
+      "page": 1,
+      "results": [],
+      "total_pages": 2,
+      "total_results": 25
+      }
+    },
+    created(){
+      this.getCharacters();
+    },
+    methods: {
+      getCharacters(){
+      axios
+      .get(this.apiUrl)
+      .then((result)=>{
+        this.results = result.data;
+      })
+      }
+    }
+  }
 </script>
 
 <style>
